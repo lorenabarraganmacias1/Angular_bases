8cd70aaed2b3c796fdc2638b3ad4b7d62696224d
Lorena Barragan <lorenabarraganmacias1@gmail.com>
2024-12-27 13:21:26 -0500

    commit second

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainPageComponent } from './pages/main-pages.component';
+import { ListComponent } from './components/list/list.component'
+
+
+
+@NgModule({
+
+  declarations: [
+
+    MainPageComponent,
+      ListComponent
+
+  ],
+  exports:[
+    MainPageComponent
+
+  ],
+
+  imports: [
+    CommonModule
+  ]
+})
+export class DbzModule { }
---
+++ src/app/dbz/pages/main-pages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-pages.component.html'
+})
+
+export class MainPageComponent {
+
+
+
+}
